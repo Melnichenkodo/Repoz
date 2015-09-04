9216fb6c390ae270aee062ec0bbb7c53c0f4c79f
melnichenkodo <melnichenkodo@gmail.com>
2015-09-04 09:42:10 +0300

    userData

--- myFirstModule/src/test/java/training/Main.java
+++ myFirstModule/src/test/java/training/Main.java
@@ -7,15 +7,15 @@ public class Main {
 
     @Test
     public void main() {
-        User user1 = new User();
+        User2.User user1 = new User2.User();
         user1.setUserName("TestUsername");
         System.out.println(user1.getUserName());
 
 
         Profile profile1 = new Profile(53, 'M', 142.64, "java developer", 'n', 'Y');
         Profile profile2 = new Profile(167, 'F', 280, "bussines analyst", 'y', 'n');
-        User user2 = new User("TestUsername", "TestPassword", profile2);
-        User user3 = new User(user2);
+        User2.User user2 = new User2.User("TestUsername", "TestPassword", profile2);
+        User2.User user3 = new User2.User(user2);
         System.out.println(user2.getUserName());
         user3.setUserName("hababalala");
         System.out.println(user3.getUserName());
